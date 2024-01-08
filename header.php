a88ee854998f35e64db3c505f482a7f1aac8af6f
Romeo Disca <romeo.disca@gmail.com>
2024-01-08 20:29:55 +0100

    change charset meta tag

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo( 'charset' ); ?>">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<link rel="profile" href="//gmpg.org/xfn/11" />
